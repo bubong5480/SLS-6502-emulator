f9505322009ba1919b0c355fb6f9255bf8877b24
BenBadnani <bbadnani@bu.edu>
2021-10-03 10:55:11 -0400

    Added base cpu struct, and preliminary while loop to emulate the nop command

--- 6502.c
+++ 6502.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+struct cpu{
+    int* pc; 
+};
+
+struct cpu* cpu_inst; 
+
 int main()
 {
-    return 0;
+    while (1);
+        switch(*(cpu_inst->pc)){
+            case 'EA':
+                *(cpu_inst->pc) += 1; 
+            default: 
+                printf('\n');
+                break;
+        }
+    return 0; 
 }
