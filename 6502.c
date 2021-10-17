283f80ffb22259d4dd6af39ceeac05572d59ab17
BenBadnani <bbadnani@bu.edu>
2021-10-17 15:31:25 -0400

    Added two switch statement cases for opcodes corresponding to different addressing modes for lda

--- 6502.c
+++ 6502.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stddef.h>
 
 int* fetch();
@@ -6,8 +7,10 @@ int* decode();
 struct cpu* execute();
 
 struct cpu{
-    int* pc; 
+    int16_t* pc; 
+    int8_t accumulator; 
 };
+
 
 struct cpu* cpu_inst;
 char *memory;
@@ -27,22 +30,72 @@ int main()
         return NULL; 
     }    
 
-    while (1){
-        switch(*(cpu_inst->pc)){
-            case 0xEA:
-                *(cpu_inst->pc) += 1; 
-            default: 
-                printf('\n');
-                break;
-        }
+    while(1){
+
     }
-    return 0; 
+
 }
 
+
+/*
+
+compartmentalizing fetch and decode
+
 int* fetch(struct cpu* cpu_inst) {
-    return(cpu_inst->pc); 
+
+    return *(cpu_inst->pc);
+}
+
+void* decode(){
+    case 0xA9:
+        char input = *(cpu_inst->pc) + 1; 
+        void* output = (*cpu_inst).accumulator
+
+        execute(input, output, 0xA9)
+}
+
+
+*/
+
+int* fetch(struct cpu* cpu_inst) {
+    switch(*(cpu_inst->pc)){
+        // LDA opcodes
+        case 0xA9: // immediate
+            *(cpu_inst->pc) += 1; // pass in incremented value or actually increment pc? 
+            (*cpu_inst).accumulator = *(cpu_inst->pc); // 
+            break;
+        case 0xA5:
+            break;
+        case 0xB5:
+            break;
+        case 0xAD: // absolute 
+            *(cpu_inst->pc) += 1;
+            char byte_1 = *(cpu_inst->pc);
+            *(cpu_inst->pc) += 1;
+            char byte_2 = *(cpu_inst->pc);
+            // two bytes to little endian , store the value of the memory address there into the accumulator 
+            int16_t mem_address = (byte_1 << 8) | byte_2;  // concatinating the two byte_values 
+            (*cpu_inst).accumulator = *(cpu_inst->pc+mem_address); // setting accumulutor to value at that mem location
+            break;
+        case 0xBD:
+            break;
+        case 0xB9:
+            break;
+        case 0xA1:
+            break;
+        case 0xB1:
+            break;
+
+        case 0xEA:
+            *(cpu_inst->pc) += 1;
+            break; 
+        default: 
+            printf('\n');
+            break;
+            
+    }
+    
+    *(cpu_inst->pc) += 1
+    return 0; 
 } 
 
-int* decode(struct cpu* cpu_inst) {
-    return(cpu_inst->pc);
-} 
