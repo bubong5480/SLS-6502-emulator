88a05332a179158b5849ea3b0a1a92cc3a087ce7
ryanv249 <47526648+ryanv249@users.noreply.github.com>
2021-10-17 13:57:23 -0400

    Revert "pulled to update local repo"
    This reverts commit b35f3bf169a6a6944af758c34a1bd39621b5a98b.

--- 6502.c
+++ 6502.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
+#include <stddef.h>
+
+int* fetch();
+int* decode();
+struct cpu* execute();
 
 struct cpu{
     int* pc; 
 };
 
-struct cpu* cpu_inst; 
+struct cpu* cpu_inst;
+char *memory;
+
+void read_in_memory_contents(char* mem_arr_pointer){
+
+    // for now, just memset every 
+}
 
 int main()
 {
-    while (1);
+    if ((memory = (char*) malloc((1 << 16) * sizeof(char))) == NULL){
+        return NULL; 
+    }
+
+    if((cpu_inst = (struct cpu*) malloc(sizeof(struct cpu))) == NULL){
+        return NULL; 
+    }    
+
+    while (1){
         switch(*(cpu_inst->pc)){
-            case 'EA':
+            case 0xEA:
                 *(cpu_inst->pc) += 1; 
             default: 
                 printf('\n');
                 break;
         }
+    }
     return 0; 
 }
+
+int* fetch(struct cpu* cpu_inst) {
+    return(cpu_inst->pc); 
+} 
+
+int* decode(struct cpu* cpu_inst) {
+    return(cpu_inst->pc);
+} 
