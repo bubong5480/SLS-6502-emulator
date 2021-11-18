0c8ad777cebcd7c125832c556957c0bdf46e7413
willwei <willwei>
2021-11-18 00:20:49 +0000

    changed uint8/16 types in structs and functions to byte/address respectively

--- src/void_functions.c
+++ src/void_functions.c
@@ -11,6 +11,7 @@
 #include "./src/uthash.h"
 
 #define byte uint8_t
+#define address uint16_t
 #define RAMSIZE (1<<16)
 #define opcode_size 147
 
@@ -19,11 +20,10 @@ struct cpu* execute();
 void build_opcode_table();
 void decode(byte);
 
-typedef void (*myFuncDef)(byte, uint8_t*);
 
 struct cpu{
-    uint16_t* pc; 
-    uint8_t accumulator; 
+    address* pc; 
+    byte accumulator; 
     byte register_x; 
     byte register_y; 
     byte status_register;
@@ -31,8 +31,8 @@ struct cpu{
 };
 
 struct opcode_table{
-    uint8_t opcodes_key;
-    myFuncDef opcode_function; 
+    byte opcodes_key;
+    byte* opcode_function; 
     UT_hash_handle hh;
 };
 
@@ -126,64 +126,64 @@ void decode(byte opcode){
     }
 } 
 
-void ADC(byte opcode, uint8_t* pc){
-    decode(opcode); 
+void ADC(byte opcode, byte* pc){
+    decode(opcode);
     OurComputer->cpu_inst->accumulator += ret.pc + ret.arg;
 };
 
 
-void AND(byte opcode, uint8_t* pc){decode(opcode); OurComputer->cpu_inst->accumulator &= ret.pc; };
-void ASL(byte opcode, uint8_t* pc){decode(opcode); ret.arg <<= 1};
-void BCC(byte opcode, uint8_t* pc){decode(opcode); *ret.pc += *ret.arg};
-void BCS(byte opcode, uint8_t* pc){if carry == 1{BCC(opcode, pc)}};
-void BEQ(byte opcode, uint8_t* pc){if zero == 1{BCC(opcode, pc)}};
-void BIT(byte opcode, uint8_t* pc){if zero == 1{BCC(opcode, pc)}};
-void BIT(byte opcode, uint8_t* pc){if N == 1{BCC(opcode, pc)}};
-void BME(byte opcode, uint8_t* pc){if zero == 0{BCC(opcode, pc)}};
-void BPL(byte opcode, uint8_t* pc){if N==0{BCC(opcode, pc)}};
-void BRK(byte opcode, uint8_t* pc){break; };
-void BVC(byte opcode, uint8_t* pc){if V==0{BCC(opcode, pc)}};
-void BVS(byte opcode, uint8_t* pc){if V==1{BCC(opcode, pc)}};
-void CLC(byte opcode, uint8_t* pc){carry = 0};
-void CLD(byte opcode, uint8_t* pc){D = 0};
-void CLI(byte opcode, uint8_t* pc){I = 0};
-void CLV(byte opcode, uint8_t* pc){V = 0};
-void CMD(byte opcode, uint8_t* pc){V = 0};
-void CPX(byte opcode, uint8_t* pc){if zero == 0{BCC(opcode, pc)}};
-void CPY(byte opcode, uint8_t* pc){if zero == 0{BCC(opcode, pc)}};
-void DEC(byte opcode, uint8_t* pc){*OurComputer->cpu_inst->pc -= 1};
-void DEX(byte opcode, uint8_t* pc){*OurComputer->cpu_inst->pc -= OurComputer->cpu_inst->register_x};
-void DEY(byte opcode, uint8_t* pc){*OurComputer->cpu_inst->pc -= OurComputer->cpu_inst->register_y};
-void EOR(byte opcode, uint8_t* pc){decode(opcode); OurComputer->accumulator ^= *ret.arg};
-void INC(byte opcode, uint8_t* pc){decode(opcode); *ret.pc += 1};
-void INX(byte opcode, uint8_t* pc){reg_x += 1};
-void INY(byte opcode, uint8_t* pc){reg_y += 1};
-void JMP(byte opcode, uint8_t* pc){decode(opcode); ret.pc = ret.arg};
-void JSR(byte opcode, uint8_t* pc){decode(opcode); ret.pc = ret.arg};
-void LDA(byte opcode, uint8_t* pc){OurComputer->accumulator = *OurComputer->cpu_inst->pc};
-void LDX(byte opcode, uint8_t* pc){reg_x = *OurComputer->cpu_inst->pc};
-void LDY(byte opcode, uint8_t* pc){reg_y = *OurComputer->cpu_inst->pc};
-void LSR(byte opcode, uint8_t* pc){decode(opcode); *ret.arg >>= 1};
-void LSR(byte opcode, uint8_t* pc){pc +=1 };
-void ORA(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
-void PHA(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
-void PHP(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
-void PLA(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
-void PLP(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
-void ROL(byte opcode, uint8_t* pc){decode(opcode); rotate *ret.arg by 1 bit left};
-void ROR(byte opcode, uint8_t* pc){decode(opcode); rotate *ret.arg by 1 bit right};
-void RTI(byte opcode, uint8_t* pc){decode(opcode); rotate *ret.arg by 1 bit right};
-void RTS(byte opcode, uint8_t* pc){decode(opcode); rotate *ret.arg by 1 bit right};
-void SBC(byte opcode, uint8_t* pc){decode(opcode); OurComputer->cpu_inst->accumulator -= *(ret.pc + ret.arg)};
-void SEC(byte opcode, uint8_t* pc){carry = 1};
-void SED(byte opcode, uint8_t* pc){D = 1};
-void SEI(byte opcode, uint8_t* pc){I = 1};
-void STA(byte opcode, uint8_t* pc){*OurComputer->cpu_inst->pc = OurComputer->cpu_inst->accumulator};
-void STX(byte opcode, uint8_t* pc){OurComputer->cpu_inst->register_x = *OurComputer->cpu_inst->pc};
-void STY(byte opcode, uint8_t* pc){OurComputer->cpu_inst->register_y = *OurComputer->cpu_inst->pc};
-void TAX(byte opcode, uint8_t* pc){OurComputer->cpu_inst->register_x = OurComputer->cpu_inst->accumulator};
-void TAY(byte opcode, uint8_t* pc){OurComputer->cpu_inst->register_y = OurComputer->cpu_inst->accumulator};
-void TSX(byte opcode, uint8_t* pc){OurComputer->cpu_inst->register_x = stack_pointer};
-void TXA(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator = OurComputer->cpu_inst->register_x};
-void TXS(byte opcode, uint8_t* pc){stack_reg = OurComputer->cpu_inst->register_x};
-void TYA(byte opcode, uint8_t* pc){OurComputer->cpu_inst->accumulator = OurComputer->cpu_inst->register_y};
+void AND(byte opcode, byte* pc) {decode(opcode); OurComputer->cpu_inst->accumulator &= ret.pc; };
+void ASL(byte opcode, byte* pc) {decode(opcode); ret.arg <<= 1};
+void BCC(byte opcode, byte* pc) {decode(opcode); *ret.pc += *ret.arg};
+void BCS(byte opcode, byte* pc) {if carry == 1{BCC(opcode, pc)}};
+void BEQ(byte opcode, byte* pc) {if zero == 1{BCC(opcode, pc)}};
+void BIT(byte opcode, byte* pc) {if zero == 1{BCC(opcode, pc)}};
+void BIT(byte opcode, byte* pc) {if N == 1{BCC(opcode, pc)}};
+void BME(byte opcode, byte* pc) {if zero == 0{BCC(opcode, pc)}};
+void BPL(byte opcode, byte* pc) {if N==0{BCC(opcode, pc)}};
+void BRK(byte opcode, byte* pc) {break; };
+void BVC(byte opcode, byte* pc) {if V==0{BCC(opcode, pc)}};
+void BVS(byte opcode, byte* pc) {if V==1{BCC(opcode, pc)}};
+void CLC(byte opcode, byte* pc) {carry = 0};
+void CLD(byte opcode, byte* pc) {D = 0};
+void CLI(byte opcode, byte* pc) {I = 0};
+void CLV(byte opcode, byte* pc) {V = 0};
+void CMD(byte opcode, byte* pc) {V = 0};
+void CPX(byte opcode, byte* pc) {if zero == 0{BCC(opcode, pc)}};
+void CPY(byte opcode, byte* pc) {if zero == 0{BCC(opcode, pc)}};
+void DEC(byte opcode, byte* pc) {*OurComputer->cpu_inst->pc -= 1};
+void DEX(byte opcode, byte* pc) {*OurComputer->cpu_inst->pc -= OurComputer->cpu_inst->register_x};
+void DEY(byte opcode, byte* pc) {*OurComputer->cpu_inst->pc -= OurComputer->cpu_inst->register_y};
+void EOR(byte opcode, byte* pc) {decode(opcode); OurComputer->accumulator ^= *ret.arg};
+void INC(byte opcode, byte* pc) {decode(opcode); *ret.pc += 1};
+void INX(byte opcode, byte* pc) {reg_x += 1};
+void INY(byte opcode, byte* pc) {reg_y += 1};
+void JMP(byte opcode, byte* pc) {decode(opcode); ret.pc = ret.arg};
+void JSR(byte opcode, byte* pc) {decode(opcode); ret.pc = ret.arg};
+void LDA(byte opcode, byte* pc) {OurComputer->accumulator = *OurComputer->cpu_inst->pc};
+void LDX(byte opcode, byte* pc) {reg_x = *OurComputer->cpu_inst->pc};
+void LDY(byte opcode, byte* pc) {reg_y = *OurComputer->cpu_inst->pc};
+void LSR(byte opcode, byte* pc) {decode(opcode); *ret.arg >>= 1};
+void LSR(byte opcode, byte* pc) {pc +=1 };
+void ORA(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
+void PHA(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
+void PHP(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
+void PLA(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
+void PLP(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator ^= *OurComputer->cpu_inst->pc};
+void ROL(byte opcode, byte* pc) {decode(opcode); rotate *ret.arg by 1 bit left};
+void ROR(byte opcode, byte* pc) {decode(opcode); rotate *ret.arg by 1 bit right};
+void RTI(byte opcode, byte* pc) {decode(opcode); rotate *ret.arg by 1 bit right};
+void RTS(byte opcode, byte* pc) {decode(opcode); rotate *ret.arg by 1 bit right};
+void SBC(byte opcode, byte* pc) {decode(opcode); OurComputer->cpu_inst->accumulator -= *(ret.pc + ret.arg)};
+void SEC(byte opcode, byte* pc) {carry = 1};
+void SED(byte opcode, byte* pc) {D = 1};
+void SEI(byte opcode, byte* pc) {I = 1};
+void STA(byte opcode, byte* pc) {*OurComputer->cpu_inst->pc = OurComputer->cpu_inst->accumulator};
+void STX(byte opcode, byte* pc) {OurComputer->cpu_inst->register_x = *OurComputer->cpu_inst->pc};
+void STY(byte opcode, byte* pc) {OurComputer->cpu_inst->register_y = *OurComputer->cpu_inst->pc};
+void TAX(byte opcode, byte* pc) {OurComputer->cpu_inst->register_x = OurComputer->cpu_inst->accumulator};
+void TAY(byte opcode, byte* pc) {OurComputer->cpu_inst->register_y = OurComputer->cpu_inst->accumulator};
+void TSX(byte opcode, byte* pc) {OurComputer->cpu_inst->register_x = stack_pointer};
+void TXA(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator = OurComputer->cpu_inst->register_x};
+void TXS(byte opcode, byte* pc) {stack_reg = OurComputer->cpu_inst->register_x};
+void TYA(byte opcode, byte* pc) {OurComputer->cpu_inst->accumulator = OurComputer->cpu_inst->register_y};
