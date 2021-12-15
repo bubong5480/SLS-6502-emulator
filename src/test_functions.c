8e5e4fa4c6c29c60b84bb12befdcae4050f1518c
willwei <>
2021-12-15 17:59:55 -0500

    Changed a print say it's binary not hex

--- src/test_functions.c
+++ src/test_functions.c
@@ -15,7 +15,7 @@ void printAccumulator() { printf("RegA: 0x%x\n", getAccumulator()); };
 void printRegisterX() { printf("RegX: 0x%x\n", getRegisterX()); };
 void printRegisterY() { printf("RegY: 0x%x\n", getRegisterY()); };
 void printHexStatusRegister() { printf("RegSF: 0x%x\n", getStatusRegister()); };
-void printBinaryStatusRegister() { printf("RegSR: 0x%s\n", toBinary(getStatusRegister(), 8)); };
+void printBinaryStatusRegister() { printf("RegSR: 0b%s\n", toBinary(getStatusRegister(), 8)); };
 void printStackPointer() { printf("RegSP: 0x%x\n", getStackPointer()); };
 void printProgramCounter() { printf("RegPC: 0x%x\n", getProgramCounter()); };
 
